b1402edc0dbc44ae7456d6e40f477d5fbe89b9b5
usama <usama@usamas-MacBook-Pro.local>
2025-07-05 17:52:11 +0500

    renaming image

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,6 +1,8 @@
 import { Link, useNavigate } from "react-router-dom";
 import { motion } from "motion/react";
 
+
+
 import AnimationWrapper from "../AnimationWrapper";
 
 export default function ClientProjectView({ data }) {
